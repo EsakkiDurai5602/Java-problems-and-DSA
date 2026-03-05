a5bd9d30fe4e08cb042f7a6743c913cab3348c20
EsakkiDurai5602 <esakkidurai5600@gmail.com>
2026-03-05 22:39:15 +0530

    InverseNumber Algorithm Added

---
+++ JavaPrograms/DSA/Algorithms/Sorting/inverseNum.java
@@ -0,0 +1,28 @@
+package Algorithms.Sorting;
+
+import java.util.*;
+
+public class inverseNum {
+
+    public static int inverseNumber(int n) {
+        int position = 1;
+        int inverse = 0;
+
+        while (n > 0) {
+            int digit = n % 10;          
+            inverse += position * Math.pow(10, digit - 1);
+            n = n / 10;                  
+            position++;
+        }
+
+        return inverse;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(inverseNumber(n));
+        sc.close();
+    }
+}
