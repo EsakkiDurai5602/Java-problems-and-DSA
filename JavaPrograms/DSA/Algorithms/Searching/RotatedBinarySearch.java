d7050bad25a743273117c7e7bb7ae8f54d0ecf7a
Esakki Durai <esakkidurai5600@gmail.com>
2026-04-29 22:57:20 +0530

    Add Rotated Binary Search implementation

---
+++ JavaPrograms/DSA/Algorithms/Searching/RotatedBinarySearch.java
@@ -0,0 +1,41 @@
+package Algorithms.Searching;
+
+public class RotatedBinarySearch {
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 7, 8};
+        int target = 6;
+
+        int ans = search(nums, target, 0, nums.length - 1);
+        System.out.println(ans);
+    }
+
+    static int search(int[] arr, int target, int start, int end) {
+        if (start > end) {
+            return -1;
+        }
+
+        int mid = start + (end - start) / 2;
+
+        if (arr[mid] == target) {
+            return mid;
+        }
+
+        // left side sorted
+        if (arr[start] <= arr[mid]) {
+            if (target >= arr[start] && target < arr[mid]) {
+                return search(arr, target, start, mid - 1);
+            } else {
+                return search(arr, target, mid + 1, end);
+            }
+        }
+
+        // right side sorted
+        else {
+            if (target > arr[mid] && target <= arr[end]) {
+                return search(arr, target, mid + 1, end);
+            } else {
+                return search(arr, target, start, mid - 1);
+            }
+        }
+    }
+}
